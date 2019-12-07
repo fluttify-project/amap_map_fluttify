e3946d7870631845f33c52c8e0dad00f06c517bf
yohom <382146139@qq.com>
2019-12-07 22:49:33 +0800

    enhance: 导出amap_core_fluttify

--- lib/amap_map_fluttify.dart
+++ lib/amap_map_fluttify.dart
@@ -1,5 +1,7 @@
 library amap_map_fluttify;
 
+export 'package:amap_core_fluttify/amap_core_fluttify.dart';
+
 export 'src/dart/amap_service.dart';
 export 'src/dart/amap_view.widget.dart';
 export 'src/dart/enums.dart';
