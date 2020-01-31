1a0fb4553cd9cfb10bc9261ab72fd83bf9635721
yohom <yohombao@gmail.com>
2020-01-31 19:20:32 +0800

    enhance: 加载图片时，做一个缓存

--- lib/src/dart/amap_controller.dart
+++ lib/src/dart/amap_controller.dart
@@ -2272,35 +2272,50 @@ class _AndroidMapDelegate extends java_lang_Object
 }
 
 mixin _Private {
+  Map<String, Uint8List> _cache = {};
+
   Future<Uint8List> _uri2ImageData(
     ImageConfiguration config,
     Uri iconUri,
   ) async {
     final imageData = Completer<Uint8List>();
-    switch (iconUri.scheme) {
-      // 网络图片
-      case 'https':
-      case 'http':
-        HttpClient httpClient = HttpClient();
-        var request = await httpClient.getUrl(iconUri);
-        var response = await request.close();
-        imageData.complete(await consolidateHttpClientResponseBytes(response));
-        break;
-      // 文件图片
-      case 'file':
-        final imageFile = File.fromUri(iconUri);
-        imageData.complete(imageFile.readAsBytesSync());
-        break;
-      // asset图片
-      default:
-        AssetImage(iconUri.path)
-            .resolve(config)
-            .addListener(ImageStreamListener((imageInfo, sync) async {
-          final byteData =
-              await imageInfo.image.toByteData(format: ImageByteFormat.png);
-          imageData.complete(byteData.buffer.asUint8List());
-        }));
-        break;
+    if (_cache.containsKey(iconUri.toString())) {
+      imageData.complete(_cache[iconUri.toString()]);
+    } else {
+      switch (iconUri.scheme) {
+        // 网络图片
+        case 'https':
+        case 'http':
+          HttpClient httpClient = HttpClient();
+          var request = await httpClient.getUrl(iconUri);
+          var response = await request.close();
+          final result = await consolidateHttpClientResponseBytes(response);
+
+          _cache[iconUri.toString()] = result;
+          imageData.complete(result);
+          break;
+        // 文件图片
+        case 'file':
+          final imageFile = File.fromUri(iconUri);
+          final result = imageFile.readAsBytesSync();
+
+          _cache[iconUri.toString()] = result;
+          imageData.complete(result);
+          break;
+        // asset图片
+        default:
+          AssetImage(iconUri.path)
+              .resolve(config)
+              .addListener(ImageStreamListener((imageInfo, sync) async {
+            final byteData =
+                await imageInfo.image.toByteData(format: ImageByteFormat.png);
+            final result = byteData.buffer.asUint8List();
+
+            _cache[iconUri.toString()] = result;
+            imageData.complete(result);
+          }));
+          break;
+      }
     }
     return imageData.future;
   }
