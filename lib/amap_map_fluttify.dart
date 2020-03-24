2bbad0785e7f15ef13f9afc3b4477b391ff97a17
yohom <yohombao@qq.com>
2020-03-24 16:14:24 +0800

    refactor: dart -> facade

--- lib/amap_map_fluttify.dart
+++ lib/amap_map_fluttify.dart
@@ -2,7 +2,7 @@ library amap_map_fluttify;
 
 export 'package:amap_core_fluttify/amap_core_fluttify.dart';
 
-export 'src/dart/amap_service.dart';
-export 'src/dart/amap_view.widget.dart';
-export 'src/dart/enums.dart';
-export 'src/dart/models.dart';
+export 'src/facade/amap_service.dart';
+export 'src/facade/amap_view.widget.dart';
+export 'src/facade/enums.dart';
+export 'src/facade/models.dart';
