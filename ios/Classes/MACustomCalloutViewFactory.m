c7e8927d313842088123c5f35a8751e7c35b3151
yohom <382146139@qq.com>
2020-05-14 16:18:21 +0800

    roll engine #57fc8440

--- ios/Classes/MACustomCalloutViewFactory.m
+++ ios/Classes/MACustomCalloutViewFactory.m
@@ -34,7 +34,8 @@ extern BOOL enableLog;
 @implementation MACustomCalloutViewPlatformView {
   int64_t _viewId;
   CGRect _frame;
-  NSDictionary<NSString *, Handler> *_handlerMap;
+  NSDictionary<NSString *, Handler>* _handlerMap;
+  MACustomCalloutView* _view;
 }
 
 - (instancetype)initWithViewId:(int64_t)viewId frame:(CGRect)frame registrar:(NSObject <FlutterPluginRegistrar> *)registrar {
@@ -49,9 +50,11 @@ extern BOOL enableLog;
 }
 
 - (UIView *)view {
-  MACustomCalloutView *view = [[MACustomCalloutView alloc] initWithFrame:_frame];
-  // 这里用一个magic number调整一下id
-  HEAP[@(2147483647 - _viewId)] = view;
+  if (_view == nil) {
+    _view = [[MACustomCalloutView alloc] initWithFrame:_frame];
+    // 这里用一个magic number调整一下id
+    HEAP[@(2147483647 - _viewId)] = _view;
+  }
 
   //region handlers
   _handlerMap = @{
@@ -164,7 +167,7 @@ extern BOOL enableLog;
     }
   }];
   //endregion
-  return view;
+  return _view;
 }
 
 //region delegate
